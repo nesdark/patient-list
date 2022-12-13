94c3705260a0619e00f70587c9974a8db9afa42a
Christian Henrique <85910247+nesdark@users.noreply.github.com>
2022-12-13 12:17:59 -0300

    Add files via upload

--- src/script.js
+++ src/script.js
@@ -12,10 +12,56 @@ const Modal = {
   },
 };
 
+function createPatient(name, age, weight, height) {
+  const patientsTable = document.querySelector('#patientsTable');
+
+  patientsTable.innerHTML += `
+    <tr>
+      <td class="name">${name}</td>
+      <td>${age}</td>
+      <td>${weight}kg</td>
+      <td>${height}m</td>
+      <td>
+        <select id="status" onchange="updateSelectToContaminated(this)">
+          <option value="normal">Normal</option>
+          <option value="contaminated">Contaminated</option>
+        </select>
+      </td>
+    </tr>
+  `;
+}
+
+function addPatient() {
+  let name = document.querySelector('#patientName');
+  let age = document.querySelector('#patientAge');
+  let weight = document.querySelector('#patientWeight');
+  let height = document.querySelector('#patientHeight');
+
+  createPatient(name.value, age.value, weight.value, height.value);
+
+  name.value = '';
+  age.value = '';
+  weight.value = '';
+  height.value = '';
+}
+
 function openModal() {
   Modal.open();
 }
 
 function closeModal() {
   Modal.close();
+}
+
+function submitInfo() {
+  Modal.close();
+  addPatient();
+  removeNoPatients();
+  updateSelectToContaminated();
+}
+
+function removeNoPatients() {
+  const noPatientsElement = document.querySelector('#no-patients');
+
+  noPatientsElement.innerHTML = '';
 }
